291bbcdf3d3d7ee3c98f77cd267f0d99863d8ab7
xuke <lovecpp@gmail.com>
2023-06-03 13:36:01 +0000

    add missing cli

--- build.sh
+++ build.sh
@@ -1,11 +1,28 @@
 
+run(){
+    docker run --privileged=true -i -v $PWD/duo:/duo -w /duo --rm milkv-build-env bash
+}
+
 get(){
-    cat <<EOF | docker run -i -v $PWD/duo:/duo -w /duo --rm milkv-build-env bash
+    cat <<EOF | run
     export REPO_URL='https://mirrors.tuna.tsinghua.edu.cn/git/git-repo'
     repo init -u https://ghproxy.com/https://github.com/milk-v/duo-manifest.git -b main -m milk-v_duo_cv180xb_sdk.xml
     repo sync
     date
 EOF
+}
+
+image(){
+
+    cat <<EOF | run
+    source build/cvisetup.sh
+    defconfig cv1800b_sophpi_duo_sd
+    clean_all
+    build_all
+    pack_sd_image
+EOF
+
+    ls -l duo/install/soc_cv1800b_sophpi_duo_sd/
 }
 
 build(){
--- ubuntu/prepare.sh
+++ ubuntu/prepare.sh
@@ -6,9 +6,13 @@ dpkg-reconfigure --frontend noninteractive tzdata
 apt install -y dialog python3-dev make git bc gcc flex bison ninja-build libssl-dev \
       rsync pkg-config device-tree-compiler squashfs-tools parted dosfstools cpio unzip wget
 
-apt install -y python-is-python3 python3-pip
+apt install -y python-is-python3 python3-pip fdisk sudo udev
 
 apt install -y cmake
 
 wget https://storage.googleapis.com/git-repo-downloads/repo -O /usr/local/bin/repo
 chmod +x /usr/local/bin/repo
+
+wget http://security.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1-1ubuntu2.1~18.04.23_amd64.deb -O ssl.deb
+dpkg -i ssl.deb
+rm ssl.deb
